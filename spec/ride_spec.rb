21d86a01ef065ae32f7ca7c3218bceddaf09d1a4
Brant Fuller <brant.fuller@gmail.com>
2021-04-15 10:07:04 -0500

    Create tests for #initialize method

---
+++ spec/ride_spec.rb
@@ -0,0 +1,16 @@
+require './lib/ride'
+
+RSpec.describe Ride do
+  describe '#initialize' do
+    ride = Ride.new({name: 'Ferris Wheel', cost: 0})
+    it 'exists' do
+      expect(ride).to be_an_instance_of(Ride)
+    end
+    it 'has a name' do
+      expect(ride.name).to eq('Ferris Wheel')
+    end
+    it 'has a cost' do
+      expect(ride.cost).to eq(0)
+    end
+  end
+end
