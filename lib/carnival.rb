bfda7c8a9c467a1003a6a112a1cc0706f18705b3
Brant Fuller <brant.fuller@gmail.com>
2021-04-15 11:24:13 -0500

    Create Carnival #attendees_by_ride_interest method

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -22,6 +22,14 @@ class Carnival
     @attendees.push(attendee)
   end
 
+  def attendees_by_ride_interest
+    hash = @rides.each_with_object([]).to_h
+    @rides.each do |ride|
+      hash[ride] = list_of_attendees_for_ride(ride)
+    end
+    hash
+  end
+
   def list_of_attendees_for_ride(ride)
     @attendees.find_all do |attendee|
       attendee.interests.include?(ride.name)
